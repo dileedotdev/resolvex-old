1d3d8ceefaa3051854e83da72953ca7975d05225
dilee <me@dilee.dev>
2023-07-27 11:10:40 +0700

    fix(web): cloudflare cache

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,10 +1,24 @@
-import { authMiddleware } from '@clerk/nextjs'
+import { authMiddleware, redirectToSignIn } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
   publicRoutes: ['/', '/(docs)(.*)'],
+  afterAuth(auth, req, evt) {
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url })
+    }
+
+    const response = NextResponse.next()
+
+    if (!auth.isPublicRoute) {
+      response.headers.set('cache-control', 'private')
+    }
+
+    return response
+  },
 })
 
 export const config = {
